a88db45d223ed581d089c55dd2a8f5442310cd3f
James Chow <james.chow@mail.adsl4less.com>
2014-04-17 18:08:33 +0100

    APParallaxHeader

--- Classes/UIScrollView+APParallaxHeader.h
+++ Classes/UIScrollView+APParallaxHeader.h
@@ -1,13 +1,41 @@
 //
 //  UIScrollView+APParallaxHeader.h
-//  TiParallaxHeader
 //
-//  Created by James Chow on 17/04/2014.
-//
+//  Created by Mathias Amnell on 2013-04-12.
+//  Copyright (c) 2013 Apping AB. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+@class APParallaxView;
+@class APParallaxShadowView;
+
 @interface UIScrollView (APParallaxHeader)
+
+- (void)addParallaxWithImage:(UIImage *)image andHeight:(CGFloat)height;
+- (void)addParallaxWithView:(UIView*)view andHeight:(CGFloat)height;
+
+@property (nonatomic, strong, readonly) APParallaxView *parallaxView;
+@property (nonatomic, assign) BOOL showsParallax;
+
+@end
+
+enum {
+    APParallaxTrackingActive = 0,
+    APParallaxTrackingInactive
+};
+
+typedef NSUInteger APParallaxTrackingState;
+
+@interface APParallaxView : UIView
+
+@property (nonatomic, readonly) APParallaxTrackingState state;
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIView *currentSubView;
+@property (nonatomic, strong) APParallaxShadowView *shadowView;
+
+@end
+
+@interface APParallaxShadowView : UIView
 
 @end
