2505fc55a810ce58774b89c1af1838d10e458351
James Chow <james.chow@mail.adsl4less.com>
2014-04-17 18:08:16 +0100

    APParallaxHeader

---
+++ Classes/UIScrollView+APParallaxHeader.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+APParallaxHeader.h
+//  TiParallaxHeader
+//
+//  Created by James Chow on 17/04/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (APParallaxHeader)
+
+@end
---
+++ Classes/UIScrollView+APParallaxHeader.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+APParallaxHeader.m
+//  TiParallaxHeader
+//
+//  Created by James Chow on 17/04/2014.
+//
+//
+
+#import "UIScrollView+APParallaxHeader.h"
+
+@implementation UIScrollView (APParallaxHeader)
+
+@end
