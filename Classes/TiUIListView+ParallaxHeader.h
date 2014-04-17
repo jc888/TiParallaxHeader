350cdb26ab63fd9b9465a2025d180d253bb8606b
James Chow <james.chow@mail.adsl4less.com>
2014-04-17 18:02:32 +0100

    Extending ListView

--- Classes/TiUIListView+ParallaxHeader.h
+++ Classes/TiUIListView+ParallaxHeader.h
@@ -6,8 +6,20 @@
 //
 //
 
-
+#import "TiUIView.h"
+#import "TiUIListViewProxy.h"
+#import "TiUIListView.h"
+#import "TiViewProxy.h"
 
 @interface TiUIListView (ParallaxHeader)
+
+@property (nonatomic, retain) UIImage * currentImage;
+@property (nonatomic, retain) TiViewProxy * currentHeaderView;
+
++ (void) swizzle;
+
+-(void)addParallaxWithView:(TiViewProxy *)headerview withHeight: (CGFloat) height;
+-(void)addParallaxWithImage:(UIImage *)image forHeight: (CGFloat) height;
+-(void)setSectionHeaderInset:(CGFloat) height;
 
 @end
--- Classes/TiUIListView+ParallaxHeader.m
+++ Classes/TiUIListView+ParallaxHeader.m
@@ -7,7 +7,77 @@
 //
 
 #import "TiUIListView+ParallaxHeader.h"
+#import "TiUtils.h"
+#import "TiUIViewProxy.h"
+#import "UIScrollView+APParallaxHeader.h"
+#import "UITableView+HeaderSectionPosition.h"
+#import "NSObject+JRSwizzle.h"
+#import <objc/runtime.h>
 
 @implementation TiUIListView (ParallaxHeader)
+
++ (void) swizzle
+{
+    NSError *error = nil;
+	[TiUIListView jr_swizzleMethod:@selector(tableView:viewForHeaderInSection:) withMethod:@selector(swizzle_tableView:viewForHeaderInSection:) error:&error];
+    
+    if (error != nil) {
+        NSLog(@"[ERROR] %@", [error localizedDescription]);
+    }
+}
+
+
+-(void)addParallaxWithView:(TiViewProxy *)headerview withHeight: (CGFloat) height
+{
+    self.currentHeaderView = headerview;
+    
+    [self.tableView addParallaxWithView: headerview.view andHeight:height];
+}
+
+-(void)addParallaxWithImage:(UIImage *)image forHeight:(CGFloat)height
+{
+    [self.tableView addParallaxWithImage:image andHeight:height];
+}
+
+-(void)setSectionHeaderInset:(CGFloat) height
+{
+    [self.tableView setHeaderViewInsets:UIEdgeInsetsMake(height, 0, 0, 0)];
+}
+
+-(void)setCurrentImage:(UIImage *)_currentImage
+{
+    objc_setAssociatedObject(self, @selector(currentImage), _currentImage, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+};
+
+-(UIImage*)currentImage
+{
+    return objc_getAssociatedObject(self, @selector(currentImage));
+}
+
+-(void)setCurrentHeaderView:(TiViewProxy *)_currentHeaderView
+{
+    objc_setAssociatedObject(self, @selector(currentHeaderView), _currentHeaderView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+};
+
+-(TiViewProxy*)currentHeaderView
+{
+    return objc_getAssociatedObject(self, @selector(currentHeaderView));
+}
+
+- (UIView *)swizzle_tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    
+    [TiUIListView jr_swizzleMethod:@selector(tableView:viewForHeaderInSection:) withMethod:@selector(swizzle_tableView:viewForHeaderInSection:) error:nil];
+    
+    //using a plain UI view we need to use tags
+    //http://stackoverflow.com/questions/20188049/uitableview-headerviewforsection-returns-null
+    
+    UIView * sectionHeView = [self tableView:tableView viewForHeaderInSection:section];
+    [sectionHeView setTag:100+section];
+    
+    [TiUIListView jr_swizzleMethod:@selector(tableView:viewForHeaderInSection:) withMethod:@selector(swizzle_tableView:viewForHeaderInSection:) error:nil];
+    
+    return sectionHeView;
+}
 
 @end
